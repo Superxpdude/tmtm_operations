3b82854923273de893ff77c376861ad95f0885c2
Superxpdude <superxpdude@gmail.com>
2017-03-20 14:02:03 -0500

    Added APC destroyed notification to daybreak.

--- daybreak.Altis/config/CfgNotifications.hpp
+++ daybreak.Altis/config/CfgNotifications.hpp
@@ -1,3 +1,18 @@
 // Notifications system
 // Allows consistent looking mission notifications to be displayed (same appearance as task notifications)
 // https://community.bistudio.com/wiki/Notification
+
+class CfgNotifications
+{
+	class apc_destroyed
+	{
+		title = "APC Destroyed";
+		iconPicture = "";
+		iconText = "";
+		description = "Command APC destroyed.<br/>Respawn Disabled";
+		color[] = {1,1,1,1};
+		duration = 5;
+		priority = 100;
+		difficulty[] = {};
+	};
+};

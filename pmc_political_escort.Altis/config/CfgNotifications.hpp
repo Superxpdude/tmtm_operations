e7a98a8cca2338710bd0ff7ec3bc73c9db35a074
Superxpdude <superxpdude@gmail.com>
2017-10-05 13:30:39 -0500

    Cleaned up branch to store single script files. Added the old version of fn_updateTask from the XP Template.

--- pmc_political_escort.Altis/config/CfgNotifications.hpp
+++
@@ -1,3 +0,0 @@
-// Notifications system
-// Allows consistent looking mission notifications to be displayed (same appearance as task notifications)
-// https://community.bistudio.com/wiki/Notification
